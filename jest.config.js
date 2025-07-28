4372494fa0200b355b95d1eb35fab6cd1cb7818e
Benjamin Po <benjaminpo212@gmail.com>
2025-07-28 09:09:50 +0800

    fix: update testEnvironment in jest.config.js to use 'jsdom'

--- jest.config.js
+++ jest.config.js
@@ -13,7 +13,7 @@ const customJestConfig = {
     '^@/components/(.*)$': '<rootDir>/components/$1',
     '^@/pages/(.*)$': '<rootDir>/pages/$1',
   },
-  testEnvironment: 'jest-environment-jsdom',
+  testEnvironment: 'jsdom',
   collectCoverageFrom: [
     '**/*.{js,jsx,ts,tsx}',
     '!**/*.d.ts',
